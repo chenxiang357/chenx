a2ee8423bd9fdedd783b7ee5d9cad0fe0dfbd2d6
chenx <chenx@DESKTOP-5RFP37B>
2017-04-08 17:54:44 +0800

    提交全部

---
+++ chenx.Log/Call_Log/Log_Interview_Singleton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace chenx.Log
+{
+    /// <summary>
+    /// 访问日志单列模式
+    /// </summary>
+    public sealed class Log_Interview_Singleton
+    {
+        private Log_Interview_Singleton()
+        {
+
+        }
+
+        public static readonly ILog_Interview Instance = new Log_Interview_DAL();
+    }
+}
